b92ea5de0818183f19127414738d68ee41ed1082
Brandolt55 <139605432+Brandolt55@users.noreply.github.com>
2023-07-16 16:10:06 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -5,15 +5,26 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login  import LoginManager
 from flask_socketio import SocketIO, emit
+import os
 
 #abrindo o site Flask
 app = Flask(__name__)
 
 #deixando o site mais seguro
 app.config['SECRET_KEY'] = '1dad613e1c42ae1b97cc863abc053ebf'
+
+
+
 #direcionando o banco de dados para o arquivo principal da pasta
-#para criar o banco de dados tem que ser um arquivo de fora da pasta junto com o main da pagina
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'comunidade.db')
+if os.getenv("DATABASE_URL"):
+    #para criar o banco de dados tem que ser um arquivo de fora da pasta junto com o main da pagina
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'comunidade.db')    
+
+
+
+
 socketio = SocketIO(app)
 
 
@@ -25,3 +36,4 @@ login_manager.login_message = 'Faça login para continuar!'
 login_manager.login_message_category = 'alert-warning'
 
 from comunidadeimpressionadora import routes
+
